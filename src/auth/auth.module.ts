a749b369294a89ff539aef1a4fdf131fb3c18c6d
sofizambrano <sizambrano@uc.cl>
2018-01-03 16:16:15 -0300

    changes up to 08-branch

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes, } from '@angular/router'; 
+
+// third-party modules
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// shared modules
+import { SharedModule } from './shared/shared.module';
+
+export const ROUTES: Routes = [
+  {
+    path: 'auth',
+    children:[
+      { path: '', pathMatch: 'full', redirectTo: 'login' },
+      { path: 'login', loadChildren:'./login/login.module#LoginModule' },
+      { path: 'register', loadChildren:'./register/register.module#RegisterModule' },
+    ]
+  }
+];
+
+export const firebaseConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyAV38A0-L2r9U71mhAA7g2tJGGQuArk8cs",
+  authDomain: "fitness-app-21d9c.firebaseapp.com",
+  databaseURL: "https://fitness-app-21d9c.firebaseio.com",
+  projectId: "fitness-app-21d9c",
+  storageBucket: "fitness-app-21d9c.appspot.com",
+  messagingSenderId: "483934226712"
+};
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(ROUTES),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    SharedModule.forRoot(),
+  ]
+})
+export class AuthModule {}
---
+++ src/health/health.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from '../auth/shared/guards/auth.guard';
+
+export const ROUTES: Routes = [
+  { path: 'schedule', canActivate: [AuthGuard], loadChildren: './schedule/schedule.module#ScheduleModule' },
+  { path: 'meals', canActivate: [AuthGuard], loadChildren: './meals/meals.module#MealsModule' },
+  { path: 'workouts', canActivate: [AuthGuard], loadChildren: './workouts/workouts.module#WorkoutsModule' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(ROUTES),
+  ]
+})
+export class HealthModule {}
